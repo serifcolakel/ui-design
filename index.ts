ee655f6b6ad3c7afbcc09fc68b9f20a7f822901b
serifcolakel <serifcolakel0@gmail.com>
2023-07-09 11:41:58 +0300

    CHANGED (serif) : recommended TypeScript version used 5.1.6 -> 5.0.0.

--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
-export { default as Button } from "./src/components/button/Button";
-export { default as Checkbox } from "./src/components/checkbox/Checkbox";
+export { default as Button } from './src/components/button/Button';
+
+export { default as Checkbox } from './src/components/checkbox/Checkbox';
+
 export { default as HorizontalScrollContainer } from './src/components/horizontal-scroll-container/HorizontalScrollContainer';
+
 export { default as Tooltip } from './src/components/tooltip/Tooltip';
+
 export { default as CustomTreeView } from './src/components/custom-tree-view/CustomTreeView';
+
 export { default as FolderView } from './src/components/folder-view/FolderView';
+
 export { default as ImageUpload } from './src/components/image-upload/ImageUpload';
